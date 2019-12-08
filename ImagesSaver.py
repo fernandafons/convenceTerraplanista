e9115a010e007a59c768623397d68f7f10f568c4
fernandafons <nanda.fonseca7@gmail.com>
2019-12-08 11:37:06 -0300

    Refatorando

--- ImagesSaver.py
+++ ImagesSaver.py
@@ -1,33 +1,27 @@
 import urllib.request
-from bs4 import BeautifulSoup as soup
+from bs4 import BeautifulSoup as Soup
 from datetime import date
 # ----------------------------------------------------------
 image_number = 1
+url_var = (date.today().strftime("%y%m%d"))
 
 
-def download_jpg(url_image, file_path, image_number):
-    full_path = file_path + str(image_number) + '.jpg'
-    urllib.request.urlretrieve(url_image, full_path)
+def download_jpg(url, file_path, doc_name):
+    full_path = file_path + str(doc_name) + '.jpg'
+    urllib.request.urlretrieve(url, full_path)
 
-
-urlvar = (date.today().strftime("%y%m%d"))
 
 while image_number <= 5:
-    my_url = 'https://apod.nasa.gov/apod/ap' + urlvar + '.html'
+    my_url = 'https://apod.nasa.gov/apod/ap' + str(url_var) + '.html'
     uClient = urllib.request.urlopen(my_url)
     page_html = uClient.read()
     uClient.close()
-    page_soup = soup(page_html, "html.parser")
-    images = page_soup.find('img')
-    images = images['src']
-
+    page_soup = Soup(page_html, "html.parser")
+    images = page_soup.find('img')['src']
     if images[:1] == 'i':
         url_image = "https://apod.nasa.gov/apod/" + images
     else:
         url_image = images
-
-    download_jpg(url_image, 'images/', image_number)
     download_jpg(url_image, 'static/', image_number)
     image_number += 1
-    urlvar = int(urlvar) - 1
-    urlvar = str(urlvar)
+    url_var = int(url_var) - 1
--- run.py
+++ run.py
@@ -1,12 +1,12 @@
-#!/usr/bin/python
-
-from flask import Flask, jsonify, render_template
-
+from flask import Flask, render_template
+# ----------------------------------------------------------
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
